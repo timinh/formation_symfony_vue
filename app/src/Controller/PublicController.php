cb9d976d62914a35809e7829b0b83e6076fdbfad
Gilles Robin <gilles.robin@uca.fr>
2025-05-15 15:51:30 +0200

    controleurs + routes + twig OK

--- app/src/Controller/PublicController.php
+++ app/src/Controller/PublicController.php
@@ -11,8 +11,21 @@ final class PublicController extends AbstractController
     #[Route('/', name: 'app_public')]
     public function index(): Response
     {
+        $testimonials = [
+            [
+                'name' => 'John Doe',
+                'position' => 'CEO of Example Company',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            ],
+            [
+                'name' => 'Jane Smith',
+                'position' => 'CTO of Example Company',
+                'content' => 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            ]
+        ];
+
         return $this->render('public/index.html.twig', [
-            'controller_name' => 'PublicController',
+            'testimonials' => $testimonials,
         ]);
     }
 }
